5fc0da5788daf7795fd0f44db68f76c7aa402dae
Citrus <zhouyou421@126.com>
2018-05-01 10:32:27 +0800

    task before

--- auto/index.js
+++ auto/index.js
@@ -1,6 +1,5 @@
-const request = require('superagent');
-// extend with Request#proxy()
-require('superagent-proxy')(request);
+const netFunc = require('./net');
+const net = new netFunc();
 
 const toolsFunc = require('./tools');
 const tools = new toolsFunc();
@@ -11,42 +10,23 @@ const dist = config.dist; //输出目录
 const collectList = config.collectList;
 const requestUrl = config.requestUrl;
 
+let downIdx = 0;
+let listDone = false;
+const asyncMax = config.asyncMax;
+let asyncCount = 0;
+let downLoadFuncs = [];
+let downTimer = null;
+let errCount = 0;
 console.log('开始获取');
 
-//let src = 'https://www.instagram.com/graphql/query/?query_id=17888483320059182&variables={"id":"45290591","first":50}';
-let getByProxy = function(src, func){
-    try{
-        return new Promise((resolve, reject) => {
-            request
-            .get(src)
-            .proxy(config.proxy)
-            .set(config.reqHeader)
-            .set("Cookie",config.cookie)
-            .end(onresponse);
-
-            function onresponse (err, res) {
-                if (err) {
-                    console.log(err);
-                    reject(err);
-                } else {
-                    resolve(res.body);
-                }
-            }
-        })
-    }catch(e){
-        console.log(e);
-    }
-}
-
-let getRes = async function(queryVar, otherParam){
+let getRes = async function(queryVar, otherParam, errBack){
     let src = requestUrl + tools.getVariables(queryVar);
-    console.log(src)
-    let param = await getByProxy(src);
+    // console.log(src)
+    let param = await net.getByProxy(src, errBack);
     let media = param.data.user.edge_owner_to_timeline_media;
     let totalCount = media.count;//总页数
     let list = media.edges;//分页信息
     let nextPage = media.page_info;
-    console.log('获取列表成功，开始下载图片');
 
     list.forEach(async (n, i) => {
         let shortCode = n.node.shortcode; // 文章短名，可拼上获得文章详情地址
@@ -57,36 +37,96 @@ let getRes = async function(queryVar, otherParam){
         //console.log(`下载：${n.node.display_url}`);
         if(fs.existsSync(downSrc)){
             // console.log(`已存在 ${downSrc}`);
+            // downIdx += 1;
+            // downList(downIdx);
             return false;
         }
-        // setTimeout(async () => {
-            let file = await getByProxy(imgRealUrl);
-            tools.saveFile(downSrc, file);
-        // }, 1000)
+
+        let fileFunc = async (errBack) => {
+            // console.log(`下载：${imgRealUrl}`);
+            let file = await net.getByProxy(imgRealUrl, errBack);
+            tools.saveFile(downSrc, file, () => {
+                asyncCount -= 1;
+            });
+            
+        }
+        downLoadFuncs.push(fileFunc);
         
     });
-
+    console.log(`获取列表成功，当前任务数：${downLoadFuncs.length}`);
+    
     //是否有下一页
     if(nextPage.has_next_page && nextPage.has_next_page != 'false'){
-        // setTimeout(() => {
+        downLoadFuncs.push(async (errBack) => {
+            asyncCount -= 1;
             getRes({
                 id: queryVar.id, 
                 first: config.pageNum, 
                 after: nextPage.end_cursor
-            }, otherParam)
-        // }, 1000 * 10);
+            }, otherParam, errBack)
+        })
+    
+    }else{
+        console.log(`${otherParam.user}已经下载完成, 总数：${totalCount}`);
+        downIdx += 1;
+        downList(downIdx);
     }
 }
 
+// 执行列队
+function startDownFuncs(){
+    for(var i = 0; i < downLoadFuncs.length; i++){
+        if(asyncCount < asyncMax){
+            let nowTask = downLoadFuncs.splice(i, 1)[0];
+            asyncCount += 1;
+            //将当前方法抽出，如果失败则重新加入列队
+            nowTask(() => {
+                // tools.log(`reTask`)
+                if(asyncCount >= 0){
+                    asyncCount -= 1;
+                }
+                
+                downLoadFuncs.push(nowTask);
+            });
+        }else{
+            break;
+        }
+    }
+    console.log(`当前任务：${downLoadFuncs.length}，并发数：${asyncCount}`);
+}
+downTimer = setInterval(() => {
+    if(listDone && downLoadFuncs.length <= 0){
+        console.log('所有任务已经完成');
+        clearInterval(downTimer);
+        return;
+    }
+    var delayTime = tools.getRandom(10 * 1000, 35 * 1000);
+    // if(downLoadFuncs.length < asyncMax){
+    //     delayTime = tools.getRandom(2 * 1000, 5 * 1000);
+    // }
+    setTimeout(() => {
+        startDownFuncs();
+    }, delayTime);
+    
+}, 3000);
 
-collectList.forEach((n, i) => {
+function downList(index){
+    let idx = index || 0;
+    let n = collectList[idx];
+    if(!n){
+        listDone = true;
+        return false;
+    }
     //检查目录是否存在，不存在则创建
     let dir = `${dist}${n.username}/`
+    console.log(`开始下载：${n.username}`);
     if(!fs.existsSync(dir)){
         fs.mkdirSync(dir, () => {
             console.log(`创建目录 ${dir}`);
         });
 
     }
-    getRes({id: n.userid, first: config.pageNum}, {dir: dir});
-})
+    getRes({id: n.userid, first: config.pageNum}, {dir: dir, user: n.username});
+}
+downList();
+
